2efe5dee31c33400a7ba237ee73807bd5395177a
Patrick Materer <patrick.materer@brandingbrand.com>
2020-07-15 13:04:31 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,184 @@
+data "aws_availability_zone" "available" {}
+
+#
+#   vpc
+#
+
+resource "aws_vpc" "main" {
+    cidr_block = var.vpc_cidr_block
+
+    tags = var.tags
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+
+    tags = var.tags
+}
+
+#
+#   public subnets
+#
+
+resource "aws_subnet" "public" {
+    count = length(var.public_subnet_cidr_blocks)
+    
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.public_subnet_cidr_blocks[count.index]
+    availability_zone = data.aws_availability_zone.available.names[count.index]
+    map_public_ip_on_launch = false
+
+    tags = var.tags
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    tags = var.tags
+}
+
+resource "aws_route_table_association" "public" {
+    count = length(var.public_subnet_cidr_blocks)
+
+    subnet_id = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route" "public" {
+    route_table_id = aws_route_table.main.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+}
+
+
+#
+#   private subnets
+#
+
+resource "aws_subnet" "private" {
+    count = length(var.private_subnet_cidr_blocks)
+    
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.private_subnet_cidr_blocks[count.index]
+    availability_zone = data.aws_availability_zone.available.names[count.index]
+    map_public_ip_on_launch = false
+
+    tags = var.tags
+}
+
+resource "aws_route_table" "private" {
+    count = length(var.private_subnet_cidr_blocks)
+
+    vpc_id = aws_vpc.main.id
+
+    tags = var.tags
+}
+
+resource "aws_route_table_association" "private" {
+    count = length(var.private_subnet_cidr_blocks)
+
+    subnet_id = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private[count.index].id
+}
+
+#
+#   nat gateways
+#
+
+resource "aws_eip" "nat_gateway" {
+    count = length(var.public_subnet_cidr_blocks)
+
+    vpc = true
+
+    tags = var.tags
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_nat_gateway" "main" {
+    count = length(var.public_subnet_cidr_blocks)
+
+    allocation_id = aws_eip.nat_gateway[count.index].id
+    subnet_id = aws_subnet.private[count.index].id
+    
+    tags = var.tags
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_route" "nat_gateway" {
+    count = length(var.public_subnet_cidr_blocks)
+
+    route_table_id = aws_route_table.private[count.index].id
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+    destination_cidr_block = "0.0.0.0/0"
+    depends_on = [
+        aws_route_table.private
+    ]
+}
+
+#
+#   nacls
+#
+
+resource "aws_network_acl" "public" {
+    vpc_id = aws_vpc.main.id
+    subnet_ids = aws_subnet.public.*.id
+
+    tags = var.tags
+}
+
+resource "aws_network_acl_rule" "public_egress_alL" {
+    network_acl_id = aws_network_acl.public.id
+    rule_number = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    egress = true
+}
+
+resource "aws_network_acl_rule" "public_ingress_alL" {
+    network_acl_id = aws_network_acl.public.id
+    rule_number = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    egress = false
+}
+
+resource "aws_network_acl" "private" {
+    vpc_id = aws_vpc.main.id
+    subnet_ids = aws_subnet.private.*.id
+
+    tags = var.tags
+}
+
+resource "aws_network_acl_rule" "private_egress_alL" {
+    network_acl_id = aws_network_acl.private.id
+    rule_number = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    egress = true
+}
+
+resource "aws_network_acl_rule" "private_ingress_alL" {
+    network_acl_id = aws_network_acl.private.id
+    rule_number = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    egress = false
+}
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "tags" {
+  type = map(string)
+
+  default = {
+    component = "infrastructure"
+    environment = "dev"
+    name = "vpc"
+    owner = "acme/devops"
+    project = "manhattan"
+    version = "v0.0.1"
+  }
+}
+
+variable "vpc_cidr_block" {
+  type = string
+
+  default = "10.0.0.0/20"
+}
+
+variable "public_subnet_cidr_blocks" {
+  type = list(string)
+
+  default = [
+    "10.0.6.0/25",
+    "10.0.6.128/25",
+    "10.0.7.0/25",
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  type = list(string)
+
+  default = [
+    "10.0.0.0/23",
+    "10.0.2.0/23",
+    "10.0.4.0/23",
+  ]
+}
