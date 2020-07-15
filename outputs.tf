8171403f469592502495f84f3be28a4747596f17
Patrick Materer <patrick.materer@brandingbrand.com>
2020-07-15 13:10:50 -0500

    Add subnet id outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
     value = aws_vpc.main.id
 }
+
+output "public_subnet_ids" {
+    value = aws_subnets.public.*.id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnets.private.*.id
+}
