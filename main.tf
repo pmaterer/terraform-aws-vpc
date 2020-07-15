61c2b4efd6d8cd8a2fe26da802e74d4ad217e853
Patrick Materer <patrick.materer@brandingbrand.com>
2020-07-15 13:22:43 -0500

    Fix argument name

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_subnet" "public" {
     
     vpc_id = aws_vpc.main.id
     cidr_block = var.public_subnet_cidr_blocks[count.index]
-    availability_zone = data.aws_availability_zone.available.names[count.index]
+    availability_zone = data.aws_availability_zone.available.name[count.index]
     map_public_ip_on_launch = false
 
     tags = local.tags
