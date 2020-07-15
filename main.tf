a8d2622b612dd16af7355432a22d5d01218852eb
Patrick Materer <patrick.materer@brandingbrand.com>
2020-07-15 13:55:57 -0500

    Fix terraform syntax

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "public" {
     
     vpc_id = aws_vpc.main.id
     cidr_block = var.public_subnet_cidr_blocks[count.index]
-    availability_zone = data.aws_availability_zone.available.name[count.index]
+    availability_zone = data.aws_availability_zones.available.name[count.index]
     map_public_ip_on_launch = false
 
     tags = local.tags
@@ -67,7 +67,7 @@ resource "aws_subnet" "private" {
     
     vpc_id = aws_vpc.main.id
     cidr_block = var.private_subnet_cidr_blocks[count.index]
-    availability_zone = data.aws_availability_zone.available.name[count.index]
+    availability_zone = data.aws_availability_zones.available.name[count.index]
     map_public_ip_on_launch = false
 
     tags = local.tags
